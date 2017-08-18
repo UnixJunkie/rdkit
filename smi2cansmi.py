7ab5ad84ce2b1d6e20c76a76606e4ce3b3fec8f2
Francois BERENGER <ligand@free.fr>
2017-08-18 14:28:49 +0900

    added smi2cansmi.py

---
+++ smi2cansmi.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2
+
+from __future__ import print_function
+
+# import numpy # to troubleshoot numpy install
+# print numpy.__path__
+
+import sys
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+from rdkit.Chem.Mol2Writer import MolToMol2Block, MolToMol2File, \
+                                  Mol2MolSupplier, Mol2Writer
+
+argc = len(sys.argv)
+if argc != 3:
+    #                    0  1          2
+    print("fatal: usage: %s <file.smi> <file.can.smi>" %
+          sys.argv[0], file=sys.stderr)
+    exit(1)
+
+mol_reader = Chem.SmilesMolSupplier(sys.argv[1])
+output = open(sys.argv[2], 'w')
+i = 0
+for mol in mol_reader:
+    try:
+        can_smi = Chem.MolToSmiles(mol)
+        name = mol.GetProp("_Name")
+        print("%s %s" % (can_smi, name), file = output)
+    except:
+        print("%s: error at index %d" % (sys.argv[0], i),
+              file=sys.stderr)
+    i = i + 1
+output.close()
